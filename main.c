db25e48e6f4806481f5f98e3d239d1ff057a23af
GalBS10 <117944971+GalBS10@users.noreply.github.com>
2022-11-22 18:00:13 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,55 @@
+#include "stdio.h"
+#include "NumClass.h"
+
+int main(){
+
+int x,y;
+
+printf("pls enter your first number: \n");
+scanf("%d",&x);
+while(x <= 0){
+printf("pls enter a number that is bigger than 0: \n");
+scanf("%d",&x);  
+}
+
+printf("pls enter your second number: \n");
+scanf("%d",&y);
+while(y < x){
+printf("pls enter a number that is bigger than the first number: \n");
+scanf("%d",&y); 
+}
+
+
+// printing prime numbers
+printf("\n \n the Prime numbers between %d and %d are: ",x,y);
+for(int i = x; i <= y; i++){
+    if(isPrime(i)){
+        printf("%d , ",i);
+    }
+}
+
+// printing armstrong nubers
+printf("\n \n the Armstrong numbers between %d and %d are: ",x,y);
+for(int i = x; i <= y; i++){
+    if(isArmstrong(i)){
+        printf("%d , ",i);
+    }
+}
+
+//printing strong numbers
+printf("\n \n the Strong numbers between %d and %d are: ",x,y);
+for(int i = x; i <= y; i++){
+    if(isStrong(i)){
+        printf("%d , ",i);
+    }
+}
+ //printing palindrome numbers
+printf("\n \n the Palindrome numbers between %d and %d are: ",x,y);
+for(int i = x; i <= y; i++){
+    if(isPalindrome(i)){
+        printf("%d , ",i);
+    }
+}
+return 0;
+}
+
